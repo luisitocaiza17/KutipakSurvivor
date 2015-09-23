b78b4a6474589d482442147f981d86a35d9f7c40
luis smartwork <luisito_caiza17@hotmail.com>
2015-09-23 17:00:03 -0500

    Procesos Palabras

--- src/java/com/motorTraduccion/IdentificadorEstructura.java
+++ src/java/com/motorTraduccion/IdentificadorEstructura.java
@@ -5,12 +5,18 @@
  */
 package com.motorTraduccion;
 
+import com.datos.DAO.PalabrasDAO;
 import java.io.FileInputStream;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
 import opennlp.tools.namefind.NameFinderME;
 import opennlp.tools.namefind.TokenNameFinderModel;
 import opennlp.tools.util.Span;
+import persistencia.tables.records.PalabrasRecord;
 
 /**
  * http://www.programcreek.com/2012/05/opennlp-tutorial/
@@ -19,76 +25,103 @@ import opennlp.tools.util.Span;
 public class IdentificadorEstructura {
     
     /*PROCESO MEDIANTE EL CUAL CADA PALABRA ES IDENTIFICADA POR SU ESTRUCTURA GRAMACTICAL*/
-    public void identificadorGenericoPalabras(String[] palabras){
+    public void identificadorGenericoPalabras(String[] palabras)throws Exception{
         /*1) Proceso de identificacion de los pronombrespropios que no se podran traducir */
         //proceso de carga masiva de identificadores.
          ArrayList IndicesDespreciados = new ArrayList();  
          ArrayList nombreI = identificadorNombresI(palabras); 
          Iterator<String> it = nombreI.iterator();
          while (it.hasNext()) {
-             IndicesDespreciados.add(it.next());             
+             IndicesDespreciados.add(it.next().toString());             
         }
-        it.remove();
+        //it.remove();
         ArrayList nombreE = identificadorNombresI(palabras);
-        it = nombreE.iterator();
-        while (it.hasNext()) {
-             IndicesDespreciados.add(it.next());             
+        Iterator<String>it2 = nombreE.iterator();
+        while (it2.hasNext()) {
+             IndicesDespreciados.add(it2.next().toString());             
         }
-        it.remove();
+        //it.remove();
         ArrayList UbicacionI = IdentificadorUbicacionesI(palabras);
-        it = UbicacionI.iterator();
-        while (it.hasNext()) {
-             IndicesDespreciados.add(it.next());             
+        Iterator<String>it3 = UbicacionI.iterator();
+        while (it3.hasNext()) {
+             IndicesDespreciados.add(it3.next().toString());             
         }
-        it.remove();
+        //it.remove();
         ArrayList UbicacionE = IdentificadorUbicacionesE(palabras);
-        it = UbicacionE.iterator();
-        while (it.hasNext()) {
-             IndicesDespreciados.add(it.next());             
+        Iterator<String>it4 = UbicacionE.iterator();
+        while (it4.hasNext()) {
+             IndicesDespreciados.add(it4.next().toString());             
         }
-        it.remove();
+        //it.remove();
         ArrayList Fecha = IdentificadorFechasI(palabras);
-        it = Fecha.iterator();
-        while (it.hasNext()) {
-             IndicesDespreciados.add(it.next());             
+        Iterator<String>it5 = Fecha.iterator();
+        while (it5.hasNext()) {
+             IndicesDespreciados.add(it5.next().toString());             
         }
-        it.remove();
+        //it.remove();
         ArrayList Tiempo = IdentificadorTiempo(palabras);
-        it = Tiempo.iterator();
-        while (it.hasNext()) {
-             IndicesDespreciados.add(it.next());             
+        Iterator<String>it6 = Tiempo.iterator();
+        while (it6.hasNext()) {
+             IndicesDespreciados.add(it6.next().toString());             
         }
-        it.remove();
+        //it.remove();
         ArrayList Moneda = IdentificadorMonetario(palabras);
-        it = Moneda.iterator();
-        while (it.hasNext()) {
-             IndicesDespreciados.add(it.next());             
+        Iterator<String>it7 = Moneda.iterator();
+        while (it7.hasNext()) {
+             IndicesDespreciados.add(it7.next().toString());             
         }
-        it.remove();
+        //it.remove();
         ArrayList NombreEspecial = IdentificadorNombresEspecialesE(palabras);
-        it = NombreEspecial.iterator();
-        while (it.hasNext()) {
-             IndicesDespreciados.add(it.next());             
+        Iterator<String>it8 = NombreEspecial.iterator();
+        while (it8.hasNext()) {
+             IndicesDespreciados.add(it8.next().toString());             
         }
-        it.remove();
+        //it.remove();
         ArrayList OrganizacionesI = IdentificadorOrganizacionesI(palabras);
-        it = OrganizacionesI.iterator();
-        while (it.hasNext()) {
-             IndicesDespreciados.add(it.next());             
+        Iterator<String>it9 = OrganizacionesI.iterator();
+        while (it9.hasNext()) {
+             IndicesDespreciados.add(it9.next().toString());             
         }
-        it.remove();
+        //it.remove();
         ArrayList OrganizacionesE = IdentificadorOrganizacionesE(palabras);
-        it = OrganizacionesE.iterator();
-        while (it.hasNext()) {
-             IndicesDespreciados.add(it.next());             
+        Iterator<String>it10 = OrganizacionesE.iterator();
+        while (it10.hasNext()) {
+             IndicesDespreciados.add(it10.next().toString());             
         }
-        it.remove();
+        //it.remove();
         //Recorrido de los identificadores
-        it = IndicesDespreciados.iterator();
-        while (it.hasNext()) {
-            System.out.println(it.next());
-                      
+        Set<String> linkedHashSet = new LinkedHashSet<String>();
+        linkedHashSet.addAll(IndicesDespreciados);
+        IndicesDespreciados.clear();
+        IndicesDespreciados.addAll(linkedHashSet);
+        Iterator<String>it11 = IndicesDespreciados.iterator();
+        while (it11.hasNext()) {
+            System.out.println(it11.next());                  
         }
+        /*Proceso de identificacion de los tipos de palabras*/
+        String[][] palabraTipos=ProcesoTiposPalabras(palabras,IndicesDespreciados);
+        for(int i=0;i<palabraTipos.length;i++){
+            System.out.println("significado: "+palabraTipos[i][0]+ " tipo palabra: "+palabraTipos[i][1]);
+        }
+    }
+    
+    public String[][] ProcesoTiposPalabras(String[] palabras, ArrayList indices ) throws Exception{
+        String[][] palabraTipos = new String[palabras.length][palabras.length];
+        for (int i=0; i<palabras.length;i++){
+            PalabrasDAO palabraProcesos= new PalabrasDAO();
+            PalabrasRecord palabraObjeto = new PalabrasRecord();
+            if(!palabraProcesos.ConsultarPalabrasExisteId(palabras[i]).equals(""))
+            {
+                palabraObjeto.setNombrepalabra(palabras[i]);
+                palabraObjeto.setIdiomaid(2);
+                List<PalabrasRecord> results = palabraProcesos.ConsultarPalabrasTraduccion(palabraObjeto);
+                for(PalabrasRecord rs : results){
+                    palabraTipos[i][0]=rs.getSignificado();
+                    palabraTipos[i][1]=""+rs.getTipoid();
+                }
+            }
+        }
+        return palabraTipos;
     }
 
     public  ArrayList identificadorNombresI(String[] tokens) {
@@ -99,9 +132,9 @@ public class IdentificadorEstructura {
             Span nameSpans[] = nameFinder.find(tokens);
             ArrayList IndicesDespreciados = new ArrayList();    
             for (int i = 0; i < nameSpans.length; i++) {
-                IndicesDespreciados.add(nameSpans[i].getStart());
+                IndicesDespreciados.add(""+nameSpans[i].getStart());
                 System.out.println("Span: " + nameSpans[i].toString());
-                System.out.println("Covered text is: " + tokens[nameSpans[i].getStart()] + " " + nameSpans[i].getStart());
+                //System.out.println("Covered text is: " + tokens[nameSpans[i].getStart()] + " " + nameSpans[i].getStart());
             }
             return IndicesDespreciados;
         } catch (Exception e) {
@@ -118,9 +151,9 @@ public class IdentificadorEstructura {
             Span nameSpans[] = nameFinder.find(tokens);
             ArrayList IndicesDespreciados = new ArrayList();    
             for (int i = 0; i < nameSpans.length; i++) {
-                IndicesDespreciados.add(nameSpans[i].getStart());
+                IndicesDespreciados.add(""+nameSpans[i].getStart());
                 System.out.println("Span: " + nameSpans[i].toString());
-                System.out.println("Covered text is: " + tokens[nameSpans[i].getStart()] + " " + nameSpans[i].getStart());
+                //System.out.println("Covered text is: " + tokens[nameSpans[i].getStart()] + " " + nameSpans[i].getStart());
             }
             return IndicesDespreciados;
         } catch (Exception e) {
@@ -140,9 +173,9 @@ public class IdentificadorEstructura {
             Span nameSpans[] = nameFinder.find(tokens);
             ArrayList IndicesDespreciados = new ArrayList();      
             for (int i = 0; i < nameSpans.length; i++) {
-                IndicesDespreciados.add(nameSpans[i].getStart());
+                IndicesDespreciados.add(""+nameSpans[i].getStart());
                 System.out.println("Span: " + nameSpans[i].toString());
-                System.out.println("Covered text is: " + tokens[nameSpans[i].getStart()] + " " + tokens[nameSpans[i].getStart() + 1]);
+                //.out.println("Covered text is: " + tokens[nameSpans[i].getStart()] + " " + tokens[nameSpans[i].getStart() + 1]);
             }
             return IndicesDespreciados;
         } catch (Exception e) {
@@ -156,15 +189,15 @@ public class IdentificadorEstructura {
             //String documentStr=new String("says to Buffalo.");
             NameFinderME nameFinder
                     = new NameFinderME(
-                            new TokenNameFinderModel(new FileInputStream("C:\\KutipakSurvivor\\librerias\\OpenNlp\\proceso\\es-ner-location")));
+                            new TokenNameFinderModel(new FileInputStream("C:\\KutipakSurvivor\\librerias\\OpenNlp\\proceso\\es-ner-location.bin")));
 
             //String tokens[] = tokenizer.tokenize(documentStr);
             Span nameSpans[] = nameFinder.find(tokens);
             ArrayList IndicesDespreciados = new ArrayList();      
             for (int i = 0; i < nameSpans.length; i++) {
-                IndicesDespreciados.add(nameSpans[i].getStart());
+                IndicesDespreciados.add(""+nameSpans[i].getStart());
                 System.out.println("Span: " + nameSpans[i].toString());
-                System.out.println("Covered text is: " + tokens[nameSpans[i].getStart()] + " " + tokens[nameSpans[i].getStart() + 1]);
+                //System.out.println("Covered text is: " + tokens[nameSpans[i].getStart()] + " " + tokens[nameSpans[i].getStart() + 1]);
             }
             return IndicesDespreciados;
         } catch (Exception e) {
@@ -178,15 +211,15 @@ public class IdentificadorEstructura {
             //String documentStr=new String("says to Buffalo.");
             NameFinderME nameFinder
                     = new NameFinderME(
-                            new TokenNameFinderModel(new FileInputStream("C:\\KutipakSurvivor\\librerias\\OpenNlp\\proceso\\en-ner-date")));
+                            new TokenNameFinderModel(new FileInputStream("C:\\KutipakSurvivor\\librerias\\OpenNlp\\proceso\\en-ner-date.bin")));
 
             //String tokens[] = tokenizer.tokenize(documentStr);
             Span nameSpans[] = nameFinder.find(tokens);
             ArrayList IndicesDespreciados = new ArrayList();      
             for (int i = 0; i < nameSpans.length; i++) {
-                IndicesDespreciados.add(nameSpans[i].getStart());
+                IndicesDespreciados.add(""+nameSpans[i].getStart());
                 System.out.println("Span: " + nameSpans[i].toString());
-                System.out.println("Covered text is: " + tokens[nameSpans[i].getStart()] + " " + tokens[nameSpans[i].getStart() + 1]);
+                //System.out.println("Covered text is: " + tokens[nameSpans[i].getStart()] + " " + tokens[nameSpans[i].getStart() + 1]);
             }
             return IndicesDespreciados;
         } catch (Exception e) {
@@ -200,15 +233,15 @@ public class IdentificadorEstructura {
             //String documentStr=new String("says to Buffalo.");
             NameFinderME nameFinder
                     = new NameFinderME(
-                            new TokenNameFinderModel(new FileInputStream("C:\\KutipakSurvivor\\librerias\\OpenNlp\\proceso\\es-ner-organization")));
+                            new TokenNameFinderModel(new FileInputStream("C:\\KutipakSurvivor\\librerias\\OpenNlp\\proceso\\es-ner-organization.bin")));
 
             //String tokens[] = tokenizer.tokenize(documentStr);
             Span nameSpans[] = nameFinder.find(tokens);
             ArrayList IndicesDespreciados = new ArrayList();      
             for (int i = 0; i < nameSpans.length; i++) {
-                IndicesDespreciados.add(nameSpans[i].getStart());
+                IndicesDespreciados.add(""+nameSpans[i].getStart());
                 System.out.println("Span: " + nameSpans[i].toString());
-                System.out.println("Covered text is: " + tokens[nameSpans[i].getStart()] + " " + tokens[nameSpans[i].getStart() + 1]);
+                //System.out.println("Covered text is: " + tokens[nameSpans[i].getStart()] + " " + tokens[nameSpans[i].getStart() + 1]);
             }
             return IndicesDespreciados;
         } catch (Exception e) {
@@ -222,15 +255,15 @@ public class IdentificadorEstructura {
             //String documentStr=new String("says to Buffalo.");
             NameFinderME nameFinder
                     = new NameFinderME(
-                            new TokenNameFinderModel(new FileInputStream("C:\\KutipakSurvivor\\librerias\\OpenNlp\\proceso\\en-ner-organization")));
+                            new TokenNameFinderModel(new FileInputStream("C:\\KutipakSurvivor\\librerias\\OpenNlp\\proceso\\en-ner-organization.bin")));
 
             //String tokens[] = tokenizer.tokenize(documentStr);
             Span nameSpans[] = nameFinder.find(tokens);
             ArrayList IndicesDespreciados = new ArrayList();      
             for (int i = 0; i < nameSpans.length; i++) {
-                IndicesDespreciados.add(nameSpans[i].getStart());
+                IndicesDespreciados.add(""+nameSpans[i].getStart());
                 System.out.println("Span: " + nameSpans[i].toString());
-                System.out.println("Covered text is: " + tokens[nameSpans[i].getStart()] + " " + tokens[nameSpans[i].getStart() + 1]);
+                //System.out.println("Covered text is: " + tokens[nameSpans[i].getStart()] + " " + tokens[nameSpans[i].getStart() + 1]);
             }
             return IndicesDespreciados;
         } catch (Exception e) {
@@ -244,15 +277,15 @@ public class IdentificadorEstructura {
             //String documentStr=new String("says to Buffalo.");
             NameFinderME nameFinder
                     = new NameFinderME(
-                            new TokenNameFinderModel(new FileInputStream("C:\\KutipakSurvivor\\librerias\\OpenNlp\\proceso\\es-ner-misc")));
+                            new TokenNameFinderModel(new FileInputStream("C:\\KutipakSurvivor\\librerias\\OpenNlp\\proceso\\es-ner-misc.bin")));
 
             //String tokens[] = tokenizer.tokenize(documentStr);
             Span nameSpans[] = nameFinder.find(tokens);
             ArrayList IndicesDespreciados = new ArrayList();      
             for (int i = 0; i < nameSpans.length; i++) {
-                IndicesDespreciados.add(nameSpans[i].getStart());
+                IndicesDespreciados.add(""+nameSpans[i].getStart());
                 System.out.println("Span: " + nameSpans[i].toString());
-                System.out.println("Covered text is: " + tokens[nameSpans[i].getStart()] + " " + tokens[nameSpans[i].getStart() + 1]);
+                //System.out.println("Covered text is: " + tokens[nameSpans[i].getStart()] + " " + tokens[nameSpans[i].getStart() + 1]);
             }
             return IndicesDespreciados;
         } catch (Exception e) {
@@ -266,15 +299,15 @@ public class IdentificadorEstructura {
             //String documentStr=new String("says to Buffalo.");
             NameFinderME nameFinder
                     = new NameFinderME(
-                            new TokenNameFinderModel(new FileInputStream("C:\\KutipakSurvivor\\librerias\\OpenNlp\\proceso\\en-ner-money")));
+                            new TokenNameFinderModel(new FileInputStream("C:\\KutipakSurvivor\\librerias\\OpenNlp\\proceso\\en-ner-money.bin")));
 
             //String tokens[] = tokenizer.tokenize(documentStr);
             Span nameSpans[] = nameFinder.find(tokens);
             ArrayList IndicesDespreciados = new ArrayList();      
             for (int i = 0; i < nameSpans.length; i++) {
-                IndicesDespreciados.add(nameSpans[i].getStart());
+                IndicesDespreciados.add(""+nameSpans[i].getStart());
                 System.out.println("Span: " + nameSpans[i].toString());
-                System.out.println("Covered text is: " + tokens[nameSpans[i].getStart()] + " " + tokens[nameSpans[i].getStart() + 1]);
+                //System.out.println("Covered text is: " + tokens[nameSpans[i].getStart()] + " " + tokens[nameSpans[i].getStart() + 1]);
             }
             return IndicesDespreciados;
         } catch (Exception e) {
@@ -288,15 +321,15 @@ public class IdentificadorEstructura {
             //String documentStr=new String("says to Buffalo.");
             NameFinderME nameFinder
                     = new NameFinderME(
-                            new TokenNameFinderModel(new FileInputStream("C:\\KutipakSurvivor\\librerias\\OpenNlp\\proceso\\en-ner-time")));
+                            new TokenNameFinderModel(new FileInputStream("C:\\KutipakSurvivor\\librerias\\OpenNlp\\proceso\\en-ner-time.bin")));
 
             //String tokens[] = tokenizer.tokenize(documentStr);
             Span nameSpans[] = nameFinder.find(tokens);
             ArrayList IndicesDespreciados = new ArrayList();      
             for (int i = 0; i < nameSpans.length; i++) {
-                IndicesDespreciados.add(nameSpans[i].getStart());
+                IndicesDespreciados.add(""+nameSpans[i].getStart());
                 System.out.println("Span: " + nameSpans[i].toString());
-                System.out.println("Covered text is: " + tokens[nameSpans[i].getStart()] + " " + tokens[nameSpans[i].getStart() + 1]);
+                //System.out.println("Covered text is: " + tokens[nameSpans[i].getStart()] + " " + tokens[nameSpans[i].getStart() + 1]);
             }
             return IndicesDespreciados;
         } catch (Exception e) {
