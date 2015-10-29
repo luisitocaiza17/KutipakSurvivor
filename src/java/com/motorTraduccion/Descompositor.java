6ced8f9e4c500a7773c6bc4d637bd86398687e7a
luis Trabajo <luis.caiza@smartwork.com.ec>
2015-10-29 14:48:32 -0500

    casi manta-desde

--- src/java/com/motorTraduccion/Descompositor.java
+++ src/java/com/motorTraduccion/Descompositor.java
@@ -36,7 +36,7 @@ public class Descompositor {
                 boolean existePlural=a.contains(palabraBuscar);
                 if(!existePlural){
                    System.out.println("no es plural");  
-                   resultado=resultado+descompositorPalabras(a,idioma)+" ";
+                   //resultado=resultado+descompositorPalabras(a,idioma)+" ";
                 }
                 else{
                     System.out.println("Es plural");
@@ -50,8 +50,28 @@ public class Descompositor {
                             a=a+cadenasPlural[i];
                         System.out.println("palabra : "+a); 
                     }
-                    resultado=resultado+descompositorPalabras(a,idioma)+" ";
-                }                     
+                    //resultado=resultado+descompositorPalabras(a,idioma)+" ";
+                }
+                palabraBuscar="MANTA ";
+                boolean existeDesde=a.contains(palabraBuscar);
+                if(!existeDesde){
+                   System.out.println("No es desde");  
+                   //resultado=resultado+descompositorPalabras(a,idioma)+" ";
+                }else{
+                    System.out.println("Si es desde ");
+                    String[] cadenasPlural = a.split("MANTA "); 
+                    a="";
+                    for (int i = 0; i < cadenasPlural.length; i++) {
+                        if(i<cadenasPlural.length-1 || i==0){
+                            a=a+cadenasPlural[i]+" MANTA ";
+                        } 
+                        else
+                            a=a+cadenasPlural[i];
+                        System.out.println("palabra : "+a); 
+                    }
+                }
+                
+                resultado=resultado+descompositorPalabras(a,idioma)+" ";
             }else
                 resultado=resultado+descompositorPalabras(a,idioma)+" ";
         }
